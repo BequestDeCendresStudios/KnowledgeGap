17b55c8677fce1d20323ca75b02c213b49f18067
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-11-14 14:04:21 +0000

    Create CoralPool.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ examples/CoralPool.rb
@@ -0,0 +1,47 @@
+require_relative "Maisettequery/Maisettequery.rb"
+require_relative "Maisettequery/Maisettelog.rb"
+require_relative "Maisettequery/Maisettevague.rb"
+
+module CoralPool
+  class IsolatedCoral
+    def self.knowledge_gaps
+      a = [ "my house",         "fruit",             "escelator entrance", "second floor item drop" ]
+      b = [ "friends house",    "vegetables",        "escelator entrance", "second floor item drop" ]
+      c = [ "sisters house",    "shoes",             "escelator entrance", "second floor item drop" ]
+      d = [ "bullys house",     "meat",              "escelator entrance", "second floor item drop" ]
+      e = [ "teachers house",   "books",             "escelator entrance", "second floor item drop" ]
+      f = [ "firemans house",   "hoses",             "escelator entrance", "second floor item drop" ]
+      g = [ "simpsons house",   "jokebooks",         "escelator entrance", "second floor item drop" ]
+      h = [ "dragons house",    "iron statues",      "escelator entrance", "second floor item drop" ]
+      i = [ "hamburgers house", "condiment shelves", "escelator entrance", "second floor item drop" ]
+
+      analyze_sabofensu(is_house_deceptive?(a[0][0], deux_chemix_ascenseur(a[0][1], a[0][2], a[0][3])),
+                        is_house_deceptive?(b[0][0], deux_chemix_ascenseur(b[0][1], b[0][2], b[0][3])),
+                        is_house_deceptive?(c[0][0], deux_chemix_ascenseur(c[0][1], c[0][2], c[0][3])),
+                        is_house_deceptive?(d[0][0], deux_chemix_ascenseur(d[0][1], d[0][2], d[0][3])),
+                        is_house_deceptive?(e[0][0], deux_chemix_ascenseur(e[0][1], e[0][2], e[0][3])),
+                        is_house_deceptive?(f[0][0], deux_chemix_ascenseur(f[0][1], f[0][2], f[0][3])),
+                        is_house_deceptive?(g[0][0], deux_chemix_ascenseur(g[0][1], g[0][2], g[0][3])),
+                        is_house_deceptive?(h[0][0], deux_chemix_ascenseur(h[0][1], h[0][2], h[0][3])),
+                        is_house_deceptive?(i[0][0], deux_chemix_ascenseur(i[0][1], i[0][2], i[0][3])))
+    end
+    
+    def self.dream_studies
+      a = [ "my house",                  "This is my house." ]
+      b = [ "friends house",    "This is my friend's house." ]
+      c = [ "sisters house",     "This is my sisters house." ]
+      d = [ "bullys house",       "This is my bullys house." ]
+      e = [ "teachers house",  "This is my teacher's house." ]
+      f = [ "firemans house",   "This is a fireman's house." ]    
+
+      # Isolated "dream" studies
+      etudie_de_yume(a[0][0], a[0][1],
+                     b[0][0], b[0][1],
+                     c[0][0], c[0][1])
+
+      etudie_de_yume(d[0][0], d[0][1],
+                     e[0][0], e[0][1],
+                     f[0][0], f[0][1])
+    end
+  end
+end
