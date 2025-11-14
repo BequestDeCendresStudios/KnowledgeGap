12bf730e21f3d096d955fb1351f4410abe5dab3c
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-11-14 14:58:19 +0000

    Update CoralPool.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- examples/CoralPool.rb
+++ examples/CoralPool.rb
@@ -26,6 +26,7 @@ module CoralPool
                         is_house_deceptive?(i[0][0], deux_chemix_ascenseur(i[0][1], i[0][2], i[0][3])))
     end
     
+    # Confirmed dreams
     def self.dream_studies
       a = [ "my house",                  "This is my house." ]
       b = [ "friends house",    "This is my friend's house." ]
@@ -34,7 +35,6 @@ module CoralPool
       e = [ "teachers house",  "This is my teacher's house." ]
       f = [ "firemans house",   "This is a fireman's house." ]    
 
-      # Isolated "dream" studies
       etudie_de_yume(a[0][0], a[0][1],
                      b[0][0], b[0][1],
                      c[0][0], c[0][1])
@@ -42,6 +42,16 @@ module CoralPool
       etudie_de_yume(d[0][0], d[0][1],
                      e[0][0], e[0][1],
                      f[0][0], f[0][1])
+    end
+    
+    # Isolated "dream" studies
+    def self.unknown_dreams
+      analyze_sabofensu(etudie_de_yume("my_house",               "This is my house.",        "friends_house",        "This is my friends house.", "sisters_house",               "This is my sisters house."),
+                        etudie_de_yume("my_school",             "This is my school.",       "friends_school",       "This is my friends school.", "sisters_school",             "This is my sisters school."),
+                        etudie_de_yume("my_church",             "This is my church.",       "friends_church",       "This is my friends church.", "sisters_church",             "This is my sisters church."),
+                        etudie_de_yume("my_grocery",           "This is my grocery.",      "friends_grocery",      "This is my friends grocery.", "sisters_grocery",           "This is my sisters grocery."),
+                        etudie_de_yume("my_garage",            " This is my garage.",       "friends_garage",       "This is my friends garage.", "sisters_garage",             "This is my sisters garage."),
+                        etudie_de_yume("my_neighborhood", "This is my neighborhood.", "friends_neighborhood", "This is my friends neighborhood.", "sisters_neighborhood", "This is my sisters neighborhood."))
     end
   end
 end
