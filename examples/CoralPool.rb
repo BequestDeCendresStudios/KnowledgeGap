4201ba12976aad3a044bf395593b656cf81271e8
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-11-15 09:34:14 +0000

    Update CoralPool.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- examples/CoralPool.rb
+++ examples/CoralPool.rb
@@ -1,6 +1,6 @@
-require_relative "Maisettequery/Maisettequery.rb"
-require_relative "Maisettequery/Maisettelog.rb"
-require_relative "Maisettequery/Maisettevague.rb"
+require_relative "Maisettequery.rb"
+require_relative "Maisettelog.rb"
+require_relative "Maisettevague.rb"
 
 module CoralPool
   class IsolatedCoral
@@ -15,15 +15,17 @@ module CoralPool
       h = [ "dragons house",    "iron statues",      "escelator entrance", "second floor item drop" ]
       i = [ "hamburgers house", "condiment shelves", "escelator entrance", "second floor item drop" ]
 
-      analyze_sabofensu(is_house_deceptive?(a[0][0], deux_chemix_ascenseur(a[0][1], a[0][2], a[0][3])),
-                        is_house_deceptive?(b[0][0], deux_chemix_ascenseur(b[0][1], b[0][2], b[0][3])),
-                        is_house_deceptive?(c[0][0], deux_chemix_ascenseur(c[0][1], c[0][2], c[0][3])),
-                        is_house_deceptive?(d[0][0], deux_chemix_ascenseur(d[0][1], d[0][2], d[0][3])),
-                        is_house_deceptive?(e[0][0], deux_chemix_ascenseur(e[0][1], e[0][2], e[0][3])),
-                        is_house_deceptive?(f[0][0], deux_chemix_ascenseur(f[0][1], f[0][2], f[0][3])),
-                        is_house_deceptive?(g[0][0], deux_chemix_ascenseur(g[0][1], g[0][2], g[0][3])),
-                        is_house_deceptive?(h[0][0], deux_chemix_ascenseur(h[0][1], h[0][2], h[0][3])),
-                        is_house_deceptive?(i[0][0], deux_chemix_ascenseur(i[0][1], i[0][2], i[0][3])))
+      #puts is_house_deceptive?(a[0][0], deux_chemix_ascenseur(a[1], a[2], a[3]))
+      
+      analyze_sabofensu(is_house_deceptive?(a[0], deux_chemix_ascenseur(a[1], a[2], a[3])),
+                        is_house_deceptive?(b[0], deux_chemix_ascenseur(b[1], b[2], b[3])),
+                        is_house_deceptive?(c[0], deux_chemix_ascenseur(c[1], c[2], c[3])),
+                        is_house_deceptive?(d[0], deux_chemix_ascenseur(d[1], d[2], d[3])),
+                        is_house_deceptive?(e[0], deux_chemix_ascenseur(e[1], e[2], e[3])),
+                        is_house_deceptive?(f[0], deux_chemix_ascenseur(f[1], f[2], f[3])),
+                        is_house_deceptive?(g[0], deux_chemix_ascenseur(g[1], g[2], g[3])),
+                        is_house_deceptive?(h[0], deux_chemix_ascenseur(h[1], h[2], h[3])),
+                        is_house_deceptive?(i[0], deux_chemix_ascenseur(i[1], i[2], i[3])))
     end
     
     # Confirmed dreams
@@ -35,23 +37,58 @@ module CoralPool
       e = [ "teachers house",  "This is my teacher's house." ]
       f = [ "firemans house",   "This is a fireman's house." ]    
 
-      etudie_de_yume(a[0][0], a[0][1],
-                     b[0][0], b[0][1],
-                     c[0][0], c[0][1])
+      etudie_de_yume(a[0], a[1],
+                     b[0], b[1],
+                     c[0], c[1])
 
-      etudie_de_yume(d[0][0], d[0][1],
-                     e[0][0], e[0][1],
-                     f[0][0], f[0][1])
+      etudie_de_yume(d[0], d[1],
+                     e[0], e[1],
+                     f[0], f[1])
     end
     
     # Isolated "dream" studies
     def self.unknown_dreams
-      analyze_sabofensu(etudie_de_yume("my_house",               "This is my house.",        "friends_house",        "This is my friends house.", "sisters_house",               "This is my sisters house."),
-                        etudie_de_yume("my_school",             "This is my school.",       "friends_school",       "This is my friends school.", "sisters_school",             "This is my sisters school."),
-                        etudie_de_yume("my_church",             "This is my church.",       "friends_church",       "This is my friends church.", "sisters_church",             "This is my sisters church."),
-                        etudie_de_yume("my_grocery",           "This is my grocery.",      "friends_grocery",      "This is my friends grocery.", "sisters_grocery",           "This is my sisters grocery."),
-                        etudie_de_yume("my_garage",            " This is my garage.",       "friends_garage",       "This is my friends garage.", "sisters_garage",             "This is my sisters garage."),
-                        etudie_de_yume("my_neighborhood", "This is my neighborhood.", "friends_neighborhood", "This is my friends neighborhood.", "sisters_neighborhood", "This is my sisters neighborhood."))
+      etudie_de_yume("my_house",                     "This is my house.",
+                     "friends_house",        "This is my friends house.",
+                     "sisters_house",        "This is my sisters house.")
+                     
+      etudie_de_yume("my_school",                    "This is my school.",
+                     "friends_school",       "This is my friends school.",
+                     "sisters_school",       "This is my sisters school.")
+      
+      etudie_de_yume("my_church",                    "This is my church.",
+                     "friends_church",       "This is my friends church.",
+                     "sisters_church",       "This is my sisters church.")
+      
+      etudie_de_yume("my_grocery",                   "This is my grocery.",
+                     "friends_grocery",      "This is my friends grocery.",
+                     "sisters_grocery",      "This is my sisters grocery.")
+      
+      etudie_de_yume("my_garage",                    " This is my garage.",
+                     "friends_garage",        "This is my friends garage.",
+                     "sisters_garage",        "This is my sisters garage.")
+      
+      etudie_de_yume("my_neighborhood",              "This is my neighborhood.",
+                     "friends_neighborhood", "This is my friends neighborhood.",
+                     "sisters_neighborhood", "This is my sisters neighborhood.")
+      
+      etudie_de_yume("my_neighborhood",              "This is my neighborhood.",
+                     "friends_neighborhood", "This is my friends neighborhood.",
+                     "sisters_neighborhood", "This is my sisters neighborhood.")
+      
+      etudie_de_yume("my_neighborhood",              "This is my neighborhood.",
+                     "friends_neighborhood", "This is my friends neighborhood.",
+                     "sisters_neighborhood", "This is my sisters neighborhood.")
+    end
+    
+    def self.show_deception
+      a = [ "my house",         "fruit",             "escelator entrance", "second floor item drop" ]
+      b = [ "friends house",    "vegetables",        "escelator entrance", "second floor item drop" ]
+      c = [ "sisters house",    "shoes",             "escelator entrance", "second floor item drop" ]
+      
+      show_decption(is_house_deceptive?(a[0], deux_chemix_ascenseur(a[1], a[2], a[3])),
+                    is_house_deceptive?(b[0], deux_chemix_ascenseur(a[1], b[2], b[3])),
+                    is_house_deceptive?(c[0], deux_chemix_ascenseur(a[1], c[2], c[3])))
     end
   end
 end
