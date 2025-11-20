b535290b47f84d0acdf0647b25cdaaac0dccf4dc
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-11-20 14:33:44 +0000

    Create structured_data.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ examples/structured_data.rb
@@ -0,0 +1,41 @@
+input_vector = [
+  [ :trivia_one,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise.",
+    0.8712000000000001*0.8712000000000001 ],
+    
+  [ 0.8712000000000001*0.8712000000000001,
+    :trivia_one,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise." ],
+
+  [ "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise.",
+    0.8712000000000001*0.8712000000000001,
+    :trivia_one ],
+], [
+  [ :trivia_two,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le bati to escelator de gauche and back to le bati.",
+    0.871200000000000*0.8712000000000001*0.8712000000000001 ],
+
+  [ 0.871200000000000*0.8712000000000001*0.8712000000000001,
+    :trivia_two,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le bati to escelator de gauche and back to le bati." ],
+    
+  [ "That house Mein maisette, is really a construct of an elevator that takes packages, from le bati to escelator de gauche and back to le bati.",
+    0.871200000000000*0.8712000000000001*0.8712000000000001,
+    :trivia_two ],  
+], [
+  [ :trivia_tre,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise.",
+    0.8712000000000001*0.8712000000000001*0.8712000000000001 ],
+    
+  [ 0.8712000000000001*0.8712000000000001*0.8712000000000001,
+    :trivia_tre,
+    "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise." ],
+    
+  [ "That house Mein maisette, is really a construct of an elevator that takes packages, from le chaise to escelator de droite and back to le chaise.",
+    0.8712000000000001*0.8712000000000001*0.8712000000000001,
+    :trivia_tre ],
+]
+
+first_trivia  = input_vector[0][0][0], input_vector[0][0][1], input_vector[0][0][2]
+second_trivia = input_vector[0][1][0], input_vector[0][1][1], input_vector[0][1][2]
+third_triviva = input_vector[0][2][0], input_vector[0][2][1], input_vector[0][2][2]
